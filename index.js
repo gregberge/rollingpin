f24a169aa4ff2ea61aaab774d00204385c763403
neoziro <neoziro@gmail.com>
2012-10-10 21:00:30 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+/*
+exports.Connection = require("./lib/connection").Connection;
+exports.Errors = require("./lib/connection").Errors;
+exports.Notification = require("./lib/notification").Notification;
+exports.Device = require("./lib/notification").Device;
+*/
